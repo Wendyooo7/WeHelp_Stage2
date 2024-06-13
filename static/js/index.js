accee782c40dbe2f94ad3db42e6c31939c22dc47
Wendyooo7 <133959213+Wendyooo7@users.noreply.github.com>
2024-06-13 18:57:33 +0800

    upade latest version

--- static/js/index.js
+++ static/js/index.js
@@ -145,143 +145,9 @@ let nextPage;
 let lastAttraction;
 let keyword;
 
-// MRT scroll bar
-async function loadMRTlist() {
-  try {
-    const response = await fetch("http://127.0.0.1:8000/api/mrts");
-    const data = await response.json();
-    const data2 = data.data;
-    console.log(data2);
-    // 裝32個捷運站名稱的陣列
-    const ulMRT = document.querySelector("ul");
-
-    for (let i = 0; i < data2.length; i++) {
-      const mrt = data2[i];
-      const liMRT = document.createElement("li"); // 創建一個新的li元素
-      liMRT.classList.add("mrt"); // 給新元素添加類別
-      liMRT.textContent = mrt;
-      ulMRT.appendChild(liMRT); // 將新元素添加到容器中
-    }
-  } catch (err) {
-    console.log(err);
-  }
-  slideBar();
-}
-
-function slideBar() {
-  const leftBtn = document.querySelector("#mrt-bar__left-btn");
-  const rightBtn = document.querySelector("#mrt-bar__right-btn");
-  const MRTlist = document.querySelector("#mrt-bar__list");
-  const ulContainer = document.querySelector("#mrt-bar__ul-container");
-
-  let currentIndex = 0;
-  const itemsToShow = 14; // 每次顯示的項目數量
-  const totalItems = MRTlist.children.length;
-
-  function updateBtns() {
-    leftBtn.disabled = currentIndex === 0;
-    rightBtn.disabled = currentIndex + itemsToShow >= totalItems;
-  }
-
-  function slideList() {
-    let offset = 0;
-    for (let i = 0; i < currentIndex; i++) {
-      offset += MRTlist.children[i].offsetWidth;
-    }
-    MRTlist.style.transform = `translateX(${-offset}px)`;
-  }
-
-  leftBtn.addEventListener("click", () => {
-    if (currentIndex > 0) {
-      currentIndex = Math.max(0, currentIndex - itemsToShow);
-      slideList();
-      updateBtns();
-    }
-  });
-
-  rightBtn.addEventListener("click", () => {
-    if (currentIndex + itemsToShow < totalItems) {
-      currentIndex = Math.min(
-        totalItems - itemsToShow,
-        currentIndex + itemsToShow
-      );
-      slideList();
-      updateBtns();
-    } else {
-      currentIndex = totalItems - itemsToShow;
-      slideList();
-      updateBtns();
-    }
-  });
-
-  updateBtns();
-}
-
-// 點MRT名稱啟動關鍵字搜尋，目前問題：搜尋不到.mrt
-document.querySelector(".mrt").addEventListener("click", async () => {
-  keyword = document.querySelector(".mrt").value;
-  console.log(keyword);
-  try {
-    const response = await fetch(
-      `http://127.0.0.1:8000/api/attractions?keyword=${encodeURIComponent(
-        keyword
-      )}`
-    );
-
-    const data = await response.json();
-    nextPage = data.nextPage;
-    const data2 = data.data;
-
-    divAttractions.innerHTML = "";
-
-    for (let i = 0; i < data2.length; i++) {
-      const name = data2[i].name;
-      const category = data2[i].category;
-      const mrt = data2[i].mrt;
-      const img = data2[i].images[0];
-
-      const divAttraction = document.createElement("div"); // 創建一個新的div元素
-      divAttraction.classList.add("attraction"); // 給新元素添加類別
-      divAttractions.appendChild(divAttraction); // 將新元素添加到容器中
-
-      const divImgName = document.createElement("div");
-      divImgName.classList.add("attraction-img-name-container");
-      divAttraction.appendChild(divImgName);
-
-      const imgAttraction = document.createElement("img");
-      imgAttraction.classList.add("img0");
-      imgAttraction.src = img;
-      divImgName.appendChild(imgAttraction);
-
-      const divName = document.createElement("div");
-      divName.classList.add("attraction__info__name");
-      divName.textContent = name;
-      divImgName.appendChild(divName);
-
-      const divInfo = document.createElement("div");
-      divInfo.classList.add("attraction__info");
-      divAttraction.appendChild(divInfo);
-
-      const infoMRT = document.createElement("div");
-      infoMRT.classList.add("attraction__info__MRT");
-      infoMRT.textContent = mrt;
-      divInfo.appendChild(infoMRT);
-
-      const infoCAT = document.createElement("div");
-      infoCAT.classList.add("attraction__info__CAT");
-      infoCAT.textContent = category;
-      divInfo.appendChild(infoCAT);
-
-      lastAttraction = divAttraction;
-    }
-
-    // 如果還有新的div產生，就把它列入觀察對象
-    if (lastAttraction) {
-      lastAttractionObserver.observe(lastAttraction);
-    }
-  } catch (err) {
-    console.log(err);
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  loadAttractions();
+  loadMRTlist();
 });
 
 // lastAttractionObserver需要定義在全域
@@ -302,11 +168,6 @@ const lastAttractionObserver = new IntersectionObserver(
     threshold: 1, // 只有當元素完全進入視窗時才觸發回調函數，預設是0，即元素一進到視窗就觸發回調函數開始載入更多內容
   }
 );
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadAttractions();
-  loadMRTlist();
-});
 
 async function loadAttractions() {
   try {
@@ -359,30 +220,19 @@ async function loadAttractions() {
     console.log(err);
   }
 
-  // console.log(lastAttraction);
-  // console.log(typeof lastAttraction);
-
   // 要放在上面迴圈結束之後，不然會重複偵測，對同個API發出多次請求
   lastAttractionObserver.observe(lastAttraction);
 }
 
-// function scrollMRTtoLeft(x, y) {
-//   const MRTlist = document.querySelector(".mrt");
-//   MRTlist.scrollBy(x, y);
-// }
-
-// function scrollMRTtoRight(x, y) {
-//   const MRTlist = document.querySelector(".mrt");
-//   MRTlist.scrollBy(x, y);
-// }
-
 async function loadMoreAttraction() {
-  keyword = document.querySelector("#banner__search_keyword").value;
+  keyword = document.querySelector("#banner__search__keyword").value;
+
   try {
     if (nextPage === null) return;
     // 這樣page=最後一頁時就會自動跳出此函式了
 
     let response;
+
     if (keyword) {
       response = await fetch(
         `http://127.0.0.1:8000/api/attractions?page=${encodeURIComponent(
@@ -396,6 +246,7 @@ async function loadMoreAttraction() {
         )}`
       );
     }
+
     const data = await response.json();
     nextPage = data.nextPage;
     const data2 = data.data;
@@ -453,7 +304,7 @@ async function loadMoreAttraction() {
 document
   .querySelector("#banner__search__btn")
   .addEventListener("click", async () => {
-    keyword = document.querySelector("#banner__search_keyword").value;
+    keyword = document.querySelector("#banner__search__keyword").value;
     try {
       const response = await fetch(
         `http://127.0.0.1:8000/api/attractions?keyword=${encodeURIComponent(
@@ -517,6 +368,114 @@ document
     }
   });
 
+async function loadMRTlist() {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/api/mrts");
+    const data = await response.json();
+    const data2 = data.data;
+    // console.log(data2);
+    // 裝32個捷運站名稱的陣列
+    const ulMRT = document.querySelector("ul");
+
+    for (let i = 0; i < data2.length; i++) {
+      const mrt = data2[i];
+      const liMRT = document.createElement("li"); // 創建一個新的li元素
+      liMRT.classList.add("mrt"); // 給新元素添加類別
+      liMRT.textContent = mrt;
+      ulMRT.appendChild(liMRT); // 將新元素添加到容器中
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const ulElement = document.querySelector("#mrt-bar__ul-container");
+
+// GPT & Phild
+function scrollToLeft() {
+  console.log("往左");
+  ulElement.scrollBy({ left: -300, behavior: "smooth" });
+}
+
+function scrollRight() {
+  console.log("往右");
+  ulElement.scrollBy({ left: 300, behavior: "smooth" });
+}
+
+const MRTlis = document.querySelectorAll(".mrt");
+console.log("MRTlis: ", MRTlis);
+
+MRTlis.forEach((li) => {
+  console.log("li: ", li);
+
+  li.addEventListener("click", async (event) => {
+    const clickedLi = event.target;
+    keyword = clickedLi.textContent;
+    console.log("keyword: ", keyword);
+
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/attractions?keyword=${encodeURIComponent(
+          keyword
+        )}`
+      );
+
+      const data = await response.json();
+      nextPage = data.nextPage;
+      const data2 = data.data;
+
+      divAttractions.innerHTML = "";
+
+      for (let i = 0; i < data2.length; i++) {
+        const name = data2[i].name;
+        const category = data2[i].category;
+        const mrt = data2[i].mrt;
+        const img = data2[i].images[0];
+
+        const divAttraction = document.createElement("div"); // 創建一個新的div元素
+        divAttraction.classList.add("attraction"); // 給新元素添加類別
+        divAttractions.appendChild(divAttraction); // 將新元素添加到容器中
+
+        const divImgName = document.createElement("div");
+        divImgName.classList.add("attraction-img-name-container");
+        divAttraction.appendChild(divImgName);
+
+        const imgAttraction = document.createElement("img");
+        imgAttraction.classList.add("img0");
+        imgAttraction.src = img;
+        divImgName.appendChild(imgAttraction);
+
+        const divName = document.createElement("div");
+        divName.classList.add("attraction__info__name");
+        divName.textContent = name;
+        divImgName.appendChild(divName);
+
+        const divInfo = document.createElement("div");
+        divInfo.classList.add("attraction__info");
+        divAttraction.appendChild(divInfo);
+
+        const infoMRT = document.createElement("div");
+        infoMRT.classList.add("attraction__info__MRT");
+        infoMRT.textContent = mrt;
+        divInfo.appendChild(infoMRT);
+
+        const infoCAT = document.createElement("div");
+        infoCAT.classList.add("attraction__info__CAT");
+        infoCAT.textContent = category;
+        divInfo.appendChild(infoCAT);
+
+        lastAttraction = divAttraction;
+      }
+
+      // 如果還有新的div產生，就把它列入觀察對象
+      if (lastAttraction) {
+        lastAttractionObserver.observe(lastAttraction);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
 // 2-3的code
 // console.log(nextPage);
 // 這邊undefined了
